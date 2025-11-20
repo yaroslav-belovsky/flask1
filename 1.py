8d4dd8cceb6e8565ec70404ed37400d2e2771b7d
Yaroslav <yaroslav.belovsky@gmail.com>
2025-11-20 21:32:00 +0200

    1

--- 1.py
+++ 1.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template, request
 import random as r
+from datetime import datetime
 
 list1 = ["Сьогодні у вас", "Завтра на вас", "Незабаром", "Колись", "Якось", "На твій день народження"]
 list2 = ["чекає", "буде", "зустріне", "настане", "почнеця", "настане", ""]
@@ -16,6 +17,14 @@ def hello_world():
 
     return 'Привіт, світе!'
 
+@app.get('/home/')
+def home():
+    user = request.args.get('user')
+    return render_template("index.html", name=user,  data=datetime.now())
+
+@app.get('/info/')
+def info():
+    return render_template("info.html")
 if __name__ == '__main__':
 
-    app.run(debug=True)
+    app.run(debug=True, port=5002)
