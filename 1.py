128d7b4488b8e599f782e287b2d7e8645725a026
yaroslav-belovsky <yaroslav.belovsky@gmail.com>
2025-12-02 18:39:50 +0200

    1

--- 1.py
+++ 1.py
@@ -5,6 +5,131 @@ from datetime import datetime
 list1 = ["Сьогодні у вас", "Завтра на вас", "Незабаром", "Колись", "Якось", "На твій день народження"]
 list2 = ["чекає", "буде", "зустріне", "настане", "почнеця", "настане", ""]
 list3 = ["гарна новина", "погана звістка", "гарний день", "поганий день", "ніхто не принесе подарунку", "щось станеться..."]
+books = ["інцеклопедія знань","Аліса в країні чудес","Аліса в задзеркаллі"]
+
+fruits_list = [{"name": "Яблуко", "color": "red"},
+{"name": "Банан", "color": "yellow"},
+{"name": "Груша", "color": "green"},
+{"name": "Вишня", "color": "red"},
+{"name": "Ківі", "color": "green"},
+{"name": "Апельсин", "color": "orange"}]
+
+types = [
+    {
+      'value': 'Сніданок',
+      'name': 'сніданок'
+    },
+{
+      'value': 'Обід',
+      'name': 'обід'
+    },
+{
+      'value': 'Вечеря',
+      'name': 'вечеря'
+    },
+{
+      'value': '4',
+      'name': 'хочу все одразу'
+    }
+]
+
+complexities = [
+    {
+      'value': 'Легка',
+      'name': 'легка'
+    },
+{
+      'value': 'Середня',
+      'name': 'середня'
+    },
+{
+      'value': 'Складна',
+      'name': 'складна'
+    },
+{
+      'value': '4',
+      'name': 'хочу все одразу'
+    }
+]
+
+food_list = [
+    {
+        "назва": "Яєчня-бовтанка (скрембл)",
+        "тип": "Сніданок",
+        "складність": "Легка"
+    },
+    {
+        "назва": "Вівсяна каша з ягодами",
+        "тип": "Сніданок",
+        "складність": "Легка"
+    },
+    {
+        "назва": "Сирники",
+        "тип": "Сніданок/Вечеря",
+        "складність": "Легка"
+    },
+    {
+        "назва": "Гречана каша з молоком",
+        "тип": "Сніданок/Вечеря",
+        "складність": "Легка"
+    },
+    {
+        "назва": "Борщ український",
+        "тип": "Обід/Вечеря",
+        "складність": "Середня"
+    },
+    {
+        "назва": "Вареники з картоплею/сиром",
+        "тип": "Обід/Вечеря",
+        "складність": "Середня"
+    },
+    {
+        "назва": "Деруни (картопляні оладки)",
+        "тип": "Сніданок/Обід/Вечеря",
+        "складність": "Легка/Середня"
+    },
+    {
+        "назва": "Салат 'Олів'є'",
+        "тип": "Обід/Вечеря",
+        "складність": "Легка/Середня"
+    },
+    {
+        "назва": "Піца 'Маргарита'",
+        "тип": "Обід/Вечеря",
+        "складність": "Середня/Складна"
+    },
+    {
+        "назва": "Стейк Рібай просмажування медіум",
+        "тип": "Вечеря",
+        "складність": "Середня/Складна"
+    },
+    {
+        "назва": "Голубці з м'ясом і рисом",
+        "тип": "Обід/Вечеря",
+        "складність": "Складна"
+    },
+    {
+        "назва": "Лазанья Болоньєзе",
+        "тип": "Обід/Вечеря",
+        "складність": "Складна"
+    },
+    {
+        "назва": "Суші та роли (домашні)",
+        "тип": "Обід/Вечеря",
+        "складність": "Складна"
+    },
+    {
+        "назва": "Бельгійські вафлі",
+        "тип": "Сніданок/Десерт",
+        "складність": "Середня"
+    },
+    {
+        "назва": "Чизкейк класичний",
+        "тип": "Десерт (Вечеря)",
+        "складність": "Складна"
+    }
+]
+
 
 app = Flask(__name__)
 
@@ -17,10 +142,29 @@ def hello_world():
 
     return 'Привіт, світе!'
 
+@app.post("/")
+def get_fruits():
+    action = request.form.get("action")
+    name = request.form.get("name")
+    color = request.form.get("color")
+    if action == "add_fruit":
+        fruits_list.append({"name": name, "color": color})
+    if action == "delete_fruit":
+        for fruit in fruits_list:
+            if fruit["name"] == name:
+                fruits_list.remove(fruit)
+
+    return render_template("fruits.html", fruits=fruits_list)
+
 @app.get('/home/')
 def home():
     user = request.args.get('user')
-    return render_template("index.html", name=user,  data=datetime.now())
+    if user is None:
+        user = "Anonim"
+    age = request.args.get('age')
+    if age is None:
+        age = 0
+    return render_template("index.html", name=user,  data=datetime.now(), age=int(age))
 
 @app.get('/info/')
 def info():
@@ -45,6 +189,24 @@ def spiderMan():
 def spiderManImage():
     return send_file('templates/marvels-spider-man-780x439.jpg', mimetype='image/png')
 
+@app.get("/get_books/")
+def get_books():
+    return render_template("books.html", books=books)
+
+@app.get("/food_menu/")
+def food():
+    return render_template("food.html", types=types, Complexity=complexities)
+
+@app.post("/food_menu/")
+def food_menu():
+    type_name = request.form.get("type")
+    complexity = request.form.get("Complexity")
+    selected = []
+    for food in food_list:
+        if complexity in food["складність"] and type_name in food["тип"]:
+            selected.append(food)
+    print(selected)
+    return selected
 
 if __name__ == '__main__':
 
