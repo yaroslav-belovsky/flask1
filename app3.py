ccc42019138dab1b0e744a3bea421bae19d96009
Yaroslav <yaroslav.belovsky@gmail.com>
2025-12-09 21:58:59 +0200

    1

--- app3.py
+++ app3.py
@@ -4,18 +4,7 @@ app = Flask(__name__)
 example = ""
 @app.get('/')
 def klkulatorGet():
-    global example
-
-    action = request.form.get('action')
-    print("hsdagjs", action)
-    if action in ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "*", "/", "+", "-"):
-        example += action
-    elif action == "=":
-        example = eval(example)
-        print("test")
-        print(example)
-        print(action)
-    return render_template('kalkulator.html', namber=example)
+    return render_template('kalkulator.html', namber="")
 
 @app.post('/')
 def klkulatorPost():
@@ -25,10 +14,10 @@ def klkulatorPost():
     if action in ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "*", "/", "+", "-"):
         example += action
     elif action == "=":
-        example = eval(action)
+        example = eval(example)
         print(example)
         print(action)
-    return render_template('kalkulator.html', namber=f"{example}")
+    return render_template('kalkulator.html', namber=example)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5002)
